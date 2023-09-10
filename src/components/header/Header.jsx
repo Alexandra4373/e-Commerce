9e7f4a0d25105b7c602d1da89c1038d8c13ed835
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-10 19:26:11 +0000

    css styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,24 @@ import header from '../../assets/header.png';
 
 const Header = () => {
   return (
-    <div className='header-logo'>
-      <img src={header} alt="header-logo"/>
+
+    <div className='header'>
+      <div className='header-logo'>
+      <img src={header} alt="header-logo" />
+      <div className='box'>
+        <h3 className='text'>New Arrival</h3>
+        <h1 className='text-'>Discover our new Collection</h1>
+        <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <button className='buy'>BUY NOW</button>
+      </div>
+      </div>
+
+      {/* <div className='box'>
+        <h3 className='text'>New Arrival</h3>
+        <h1 className='text-'>Discover our new Collection</h1>
+        <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        <button className='buy'>BUY NOW</button>
+      </div> */}
       
     </div>
   )
