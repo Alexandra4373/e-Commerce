c2f1da97a65757e5386e0a7415868358e0ced0b6
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-10 19:57:20 +0000

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import './header.css'
-import header from '../../assets/header.png';
+import React from "react";
+import "./header.css";
+import header from "../../assets/header.png";
 
 const Header = () => {
-  return (
-
-    <div className='header'>
-      <div className='header-logo'>
+	return (
+		<div className="header">
+			{/* <div className='header-logo'>
       <img src={header} alt="header-logo" />
       <div className='box'>
         <h3 className='text'>New Arrival</h3>
@@ -14,17 +13,31 @@ const Header = () => {
         <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
         <button className='buy'>BUY NOW</button>
       </div>
-      </div>
+      </div> */}
 
-      {/* <div className='box'>
+			{/* <div className='box'>
         <h3 className='text'>New Arrival</h3>
         <h1 className='text-'>Discover our new Collection</h1>
         <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
         <button className='buy'>BUY NOW</button>
       </div> */}
-      
-    </div>
-  )
-}
 
-export default Header
+			<div className="header-image">
+				<div className="header-left"></div>
+				<div className="header-right">
+					<div className="box">
+						<h3 className="text">New Arrival</h3>
+						<h1 className="text-">Discover our new Collection</h1>
+						<p className="text--">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+							tellus, luctus nec ullamcorper mattis.
+						</p>
+						<button className="buy">BUY NOW</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
