8e3a63e6bec6618b129dc3f6d6a697e105481e19
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-10 16:12:17 +0000

    working on navbar component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './App.css';
 
 // import {Browse, Explore, Footer, Header, Navbar, Products, Share} from './components';
-import Navbar from './components';
+import { Navbar } from './components';
 // import {About, Shop} from './containers';
 
 const App = () => {
   return (
     <div className="App">    
-    <div>
       <Navbar/>
-    </div>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import './navbar.css'
+import logoIcon from '../../assets/logo.png'; 
+import logo from '../../assets/SkinClinic.png';
+import profile from '../../assets/profile.png';
+import search from '../../assets/search.png';
+import like from '../../assets/like.png';
+import cart from '../../assets/cart.png';
+
 
 const Navbar = () => {
   return (
-    <div>
+      <nav className="nav-bar">
+      <div className="navbar-links">
+      <div className="logo">
+        <img src={logoIcon} alt="logo" className="logo-icon" />
+        <img src={logo} alt="logo" className="logo-" />
+        <img src={profile} alt="logo" />
+        <img src={search} alt="logo" />
+        <img src={like} alt="logo" />
+        <img src={cart} alt="logo"  />
+        {/* </div>
+      <div className="navbar-links_container"> */}
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">Shop</a></p>
+          <p><a href="#possibility">About</a></p>
+          <p><a href="#features">Contact</a></p>
+        </div>
+        </div>  
+     </nav>
       
-    </div>
+      
+   
   )
 }
 
