7f083daf5ee24aacc8443ab49cf9041e73d3b8be
Jonathan Kofi Donkor <jkd6735@gmail.com>
2023-09-10 20:00:08 +0000

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,36 +4,24 @@ import header from "../../assets/header.png";
 
 const Header = () => {
 	return (
-		<div className="header">
-			{/* <div className='header-logo'>
-      <img src={header} alt="header-logo" />
-      <div className='box'>
-        <h3 className='text'>New Arrival</h3>
-        <h1 className='text-'>Discover our new Collection</h1>
-        <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-        <button className='buy'>BUY NOW</button>
-      </div>
-      </div> */}
+		// Header image
+		<div className="header-image">
+			{/* Two divisons */}
 
-			{/* <div className='box'>
-        <h3 className='text'>New Arrival</h3>
-        <h1 className='text-'>Discover our new Collection</h1>
-        <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-        <button className='buy'>BUY NOW</button>
-      </div> */}
+			{/* Left division is empty */}
+			<div className="header-left"></div>
 
-			<div className="header-image">
-				<div className="header-left"></div>
-				<div className="header-right">
-					<div className="box">
-						<h3 className="text">New Arrival</h3>
-						<h1 className="text-">Discover our new Collection</h1>
-						<p className="text--">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-							tellus, luctus nec ullamcorper mattis.
-						</p>
-						<button className="buy">BUY NOW</button>
-					</div>
+			{/* Right division */}
+			<div className="header-right">
+				{/* Box, align items center */}
+				<div className="box">
+					<h3 className="text">New Arrival</h3>
+					<h1 className="text-">Discover our new Collection</h1>
+					<p className="text--">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+						tellus, luctus nec ullamcorper mattis.
+					</p>
+					<button className="buy">BUY NOW</button>
 				</div>
 			</div>
 		</div>
