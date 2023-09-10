eb18553ab81917aaedd2289b342fc2a05a0df8f2
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-10 20:30:50 +0000

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import './header.css'
-import header from '../../assets/header.png';
+import React from "react";
+import "./header.css";
+// import header from "../../assets/header.png";
 
 const Header = () => {
-  return (
+	return (
+		// Header image
+		<div className="header-image">
+			{/* Two divisons */}
 
-    <div className='header'>
-      <div className='header-logo'>
-      <img src={header} alt="header-logo" />
-      <div className='box'>
-        <h3 className='text'>New Arrival</h3>
-        <h1 className='text-'>Discover our new Collection</h1>
-        <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-        <button className='buy'>BUY NOW</button>
-      </div>
-      </div>
+			{/* Left division is empty */}
+			<div className="header-left"></div>
 
-      {/* <div className='box'>
-        <h3 className='text'>New Arrival</h3>
-        <h1 className='text-'>Discover our new Collection</h1>
-        <p className='text--'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-        <button className='buy'>BUY NOW</button>
-      </div> */}
-      
-    </div>
-  )
-}
+			{/* Right division */}
+			<div className="header-right">
+				{/* Box, align items center */}
+				<div className="box">
+					<h3 className="text">New Arrival</h3>
+					<h1 className="text-">Discover our new Collection</h1>
+					<p className="text--">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+						tellus, luctus nec ullamcorper mattis.
+					</p>
+					<button className="buy">BUY NOW</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
