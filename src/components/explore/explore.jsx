6963af092ffca44e94d02e0c2c3602e8b6192c22
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-15 16:55:02 +0000

    update

--- src/components/explore/explore.jsx
+++ src/components/explore/explore.jsx
@@ -1,30 +1,46 @@
-import React from 'react'
-import './explore.css';
+import React from "react"
+import "./explore.css";
 import Crendenza from '../../assets/Credenza.png'
 import ChaiseLounge from '../../assets/Chaise Lounge.png'
 import ChinaCabinet from '../../assets/China Cabinet.png'
+import RightArrowIcon from '../../assets/right arrow.png'
 
-const explore = () => {
+
+
+const Explore = () => {
   return (
-    <div className='explore_container'>
-      <div className='explore_align-left'>
+      <div className='explore_container'>
+      <div className='explore_content'>
         <h1>50+ Beautiful rooms inspiration</h1>
-        <p>Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
+        <p >Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
 
-        <button>Explore more</button>
+        <button type='button' className='explore__button'>Explore more</button>
       </div>
 
       <div className='container'>
-        <p>01 <br/> Bed Room</p>
+        <div className='sub-container'>
+        <p>01 <hr/> Bed Room</p>
         <h2>Inner Peace</h2>
+        <img src={RightArrowIcon} alt='logo' />
+        </div>
 
-        <img src={Crendenza} alt='logo'/>
-        <img src={ChaiseLounge} alt='logo'/>
-        <img src={ChinaCabinet} alt='logo'/>
+        <div className='explore__gallery-images'>
+        <img src={Crendenza} alt='logo' />
+        {/* <img src={RightArrowIcon} alt='logo'  className="arrow-bottom"/> */}
+        <img src={ChaiseLounge} alt='logo' className="gallery-image__height" />
+        <img src={ChinaCabinet} alt='logo' />  
+          
+       
       </div>
+
+
+      
+    </div>
+  
       
     </div>
   )
 }
 
-export default explore
+export default Explore
+
