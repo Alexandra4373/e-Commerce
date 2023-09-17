196b44d6bf4743d88ae9c83d0077e048c3e06f6d
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-17 20:08:34 +0000

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,2 @@
 export {default as Navbar} from "./navbar/Navbar";
-// export {default as Browse} from "./browse/Browse";
-export {default as Explore} from "./explore/Explore";
 export {default as Footer} from "./footer/Footer";
-export {default as Header} from "./header/Header";
-export {default as Products} from "./products/Products";
-export {default as Share} from "./share/Share";
